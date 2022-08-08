e09b453f4e330ac4f2fd4dc3367289e65bca19f8
2xInG <zhangxing91@foxmail.com>
2022-08-08 08:51:51 +0800

    new tag

--- ziface/iconnection.go
+++ ziface/iconnection.go
@@ -11,6 +11,9 @@
 // @Title  iconnection.go
 // @Description  全部连接相关方法声明
 // @Author  Aceld - Thu Mar 11 10:32:29 CST 2019
+// @Description  新增获取客户端心跳掉线检测（SetDeadTime GetDeadTime）
+// @Author  2xInG - 2022年8月8日08:51:19
+
 package ziface
 
 import (
@@ -20,20 +23,20 @@ import (
 
 //定义连接接口
 type IConnection interface {
-	Start() 			//启动连接，让当前连接开始工作
-	Stop() 				//停止连接，结束当前连接状态M
-	Context() context.Context 		//返回ctx，用于用户自定义的go程获取连接退出状态
+	Start()                   //启动连接，让当前连接开始工作
+	Stop()                    //停止连接，结束当前连接状态M
+	Context() context.Context //返回ctx，用于用户自定义的go程获取连接退出状态
 
 	GetTCPConnection() *net.TCPConn //从当前连接获取原始的socket TCPConn
-	GetConnID() uint32 				//获取当前连接ID
-	RemoteAddr() net.Addr 			//获取远程客户端地址信息
+	GetConnID() uint32              //获取当前连接ID
+	RemoteAddr() net.Addr           //获取远程客户端地址信息
 
-	SendMsg(msgID uint32, data []byte) error 		//直接将Message数据发送数据给远程的TCP客户端(无缓冲)
-	SendBuffMsg(msgID uint32, data []byte) error	//直接将Message数据发送给远程的TCP客户端(有缓冲)
+	SendMsg(msgID uint32, data []byte) error     //直接将Message数据发送数据给远程的TCP客户端(无缓冲)
+	SendBuffMsg(msgID uint32, data []byte) error //直接将Message数据发送给远程的TCP客户端(有缓冲)
 
-	SetProperty(key string, value interface{}) 		//设置链接属性
-	GetProperty(key string) (interface{}, error)	//获取链接属性
-	RemoveProperty(key string) 						//移除链接属性
-	GetDeadTime() *int64                            //获取哏屁时间
-	SetDeadTime(int64)                              //设置哏屁时间
+	SetProperty(key string, value interface{})   //设置链接属性
+	GetProperty(key string) (interface{}, error) //获取链接属性
+	RemoveProperty(key string)                   //移除链接属性
+	GetDeadTime() *int64                         //获取哏屁时间
+	SetDeadTime(int64)                           //设置哏屁时间
 }
